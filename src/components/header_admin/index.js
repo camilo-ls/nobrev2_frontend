e7924cd926c299b475e2f07736f8380930538d2d
Camilo Sidou <cls@icomp.ufam.edu.br>
2020-07-13 07:03:51 -0400

    primeiro commit

---
+++ src/components/header_admin/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HeaderAdmin() {
+  return (
+    <h1>Cabeçalho</h1>
+  );
+}
+
+export default HeaderAdmin;
---
+++ src/views/admin/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Componentes custom:
+import HeaderAdmin from '../../components/header_admin'
+import AdminFormTable from '../../components/admin_form_table'
+
+function Admin() {
+  return (
+    <div>
+      <HeaderAdmin />
+      <AdminFormTable />
+    </div>
+  );
+}
+
+export default Admin;
